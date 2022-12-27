2894cbf14370ac0980fce44617a2925f49ac75c8
vikasz1 <mauryvikas12345@gmail.com>
2022-12-28 00:47:30 +0530

    Pagination added

--- vidly/src/components/Movie.jsx
+++ vidly/src/components/Movie.jsx
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import "../services/fakeMovieService";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1,
   };
   handleDelete(movie) {
     console.log(movie);
@@ -16,45 +20,26 @@ class Movies extends Component {
     // console.log(movies);
     const index = movies.indexOf(movie);
     // console.log(index);
-    console.log(movies[index])
+    console.log(movies[index]);
     movies[index] = { ...movies[index] };
     movies[index].like = !movies[index].like;
-    console.log(movies[index])
+    console.log(movies[index]);
     this.setState({ movies: movies });
   }
-
-  renderItems() {
-    const movies = this.state.movies;
-    return movies.map((entry) => {
-      return (
-        <tr key={entry._id}>
-          <td>{entry.title}</td>
-          <td>{entry.genre.name}</td>
-          <td>{entry.numberInStock}</td>
-          <td>{entry.dailyRentalRate}</td>
-          <td>
-            <Like liked={entry.like} onClick={() => this.handleLike(entry)} />
-          </td>
-          <td>
-            <button
-              className="btn btn-danger"
-              onClick={() => this.handleDelete(entry)}
-            >
-              Delete
-            </button>{" "}
-          </td>
-        </tr>
-      );
-    });
-  }
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+ 
   render() {
-    // console.log(getMovies());
-    const movies = this.state.movies;
-    if (this.state.movies.length === 0)
-      return <p>There are no movies in the database.</p>;
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const movies = paginate(allMovies, currentPage, pageSize);
+    const curr_movies = [...movies]
+    console.log(curr_movies);
+    if (count === 0) return <p>There are no movies in the database.</p>;
     return (
       <>
-        <p>Showing {this.state.movies.length} movies in the database.</p>
+        <p>Showing {count} movies in the database.</p>
         <table className="table">
           <thead>
             <tr>
@@ -66,11 +51,42 @@ class Movies extends Component {
               <th></th>
             </tr>
           </thead>
-          <tbody>{this.renderItems()}</tbody>
+          <tbody>
+            {curr_movies.map((movie) => {
+              console.log(movie.title)
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.like}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => this.handleDelete(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          currentPage={currentPage}
+        />
       </>
     );
   }
 }
-
 export default Movies;
